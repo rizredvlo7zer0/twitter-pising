95cf423ce8a23909b987a798ff2380ee7a5c78db
rizredvlo7zer0 <50101349+rizredvlo7zer0@users.noreply.github.com>
2020-02-05 03:48:30 +0000

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
- <?php
-<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.1//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
@@ -84,4 +82,3 @@
 
   </body>
 </html>
-<?php } ?>
